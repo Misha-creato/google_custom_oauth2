b0afe133c202be1c255e664075a1df815ee322f6
Iri O <oparina.iri@gmail.com>
2024-10-23 20:43:41 +0500

    Removed descriptions, added summary

--- setup.py
+++ setup.py
@@ -1,5 +1,6 @@
 from setuptools import setup, find_packages
 
+
 with open('requirements.txt') as f:
     requirements = f.read().splitlines()
 
@@ -7,18 +8,11 @@ with open('requirements.txt') as f:
 setup(
     name="google_custom_oauth2",
     version="0.1.0",
-    description="Google oauth2 authorization library",
-    long_description=open("README.md").read(),
-    long_description_content_type="text/markdown",
+    summary="Google oauth2 authorization library",
     author="Rina",
     author_email="oparina.iri@gmail.com",
     url="https://github.com/Misha-creato/google_custom_oauth2",
     packages=find_packages(),
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
     python_requires='>=3.6',
     install_requires=requirements,
 )
